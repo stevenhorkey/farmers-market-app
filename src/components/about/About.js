68bcf67ea8b24aedf47f4c9033082fbac1880655
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-06-16 12:59:21 -0700

    index pages for components to export

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 import './About.css';
 
 class About extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <section className="about">
                 <div className="container">
                     <div className="row">
-                    <div className="col-lg-8 col-md-10 mx-auto">
-                        <div className="about-heading">
-                            <h1>About</h1>
-                            <span className="subheading">A Blog Theme by Start Bootstrap</span>
+                        <div className="col-lg-8 col-md-10 mx-auto">
+                            <div className="about-heading">
+                                <h1>About</h1>
+                                <span className="subheading">Welcome! At it's core, Farmer's Market App is a platform to connect individuals to local markets and farmers. The aim of this app is to get fresh, wholesome food into the hands of local communities. </span>
+                            </div>
+                            <p></p>
                         </div>
-                        <p></p>
-                    </div>
                     </div>
                 </div>
             </section>
