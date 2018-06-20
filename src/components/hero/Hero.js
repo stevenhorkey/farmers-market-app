655bb40b78888dc1ea2bf50c511920960256d02f
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-06-19 20:30:16 -0700

    components

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './Hero.css';
+
+class Hero extends Component {
+    render() {
+        return (
+            // Page Header
+            <header className="masthead">
+                <div className="overlay"></div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 col-md-10 mx-auto">
+                            <div className="site-heading">
+                                <h1>FM</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Hero;
+
