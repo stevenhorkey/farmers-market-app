3923089407275dae8861693cd869931bfbaa48ce
Patricia Martinez <trish.on.a.mish@gmail.com>
2018-06-19 21:27:29 -0700

    update pages and components

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -7,12 +7,12 @@ import UpcomingMarkets from '../../components/upcomingMarkets/UpcomingMarkets'
 import About from '../../components/about/About'
 
 class Home extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Hero siteName={this.props.siteName} />
-                <UpcomingMarkets/>
-                <About/>
+                <UpcomingMarkets />
+                <About />
             </div>
         )
     }
