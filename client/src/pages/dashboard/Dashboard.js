cde557bead205e8a168ba5e3b752766ced097fd0
killianmh <killianmh@gmail.com>
2018-06-22 11:43:04 -0700

    added pseudocode to dashboard page

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,13 +1,43 @@
-// navbar
-//Are you a fuckin seller or organizer?
+//SELLER    
+//sidebar (in the container not the root)
+   //manage products
+       //initially generates all of the users products on the site
+       //add products button
+           //modal form
+               // item name (required)
+               // item pic (optional)
+               // item description (optional)
+               // item price (optional)
+       //hover picture brings x to delete product
+       //click product allows user to edit products
+           //modal form
+               // item name (required)
+               // item pic (optional)
+               // item description (optional)
+               // item price (optional)
+   //connect to market
+       //location input
+       //generates list of markets to associate ith
+   //manage profile
+       //create mission statement
+       //profile picture
 
-//seller
-    //add products
-    //and photos
-    //add which market and location
-    //farmer profile
-
-//organizer 
-    //create market
-    //sellers
-    //validate or remove sellers (use tax id?)
+//ORGANIZER
+   //sidebar
+       //farmer request (notification)
+           //list of farmers who have associated themselves with the    market
+               //accept or deny button for each
+       //manage market
+           //if market does not exist
+               //create market button
+                   //modal form
+                       //time
+                       //name
+                       //image
+                       //location
+               //manage market
+                   //modal form
+                       //time
+                       //image
+                       //name
+                       //location
