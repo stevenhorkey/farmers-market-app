bb2bc18043e1aaa217c722ec1f6f3f35d3e63776
killianmh <killianmh@gmail.com>
2018-07-07 11:29:47 -0700

    product cards for dashboard vs products page

--- client/src/components/products/product/Product.js
+++ client/src/components/products/product/Product.js
@@ -5,25 +5,43 @@ class Product extends Component {
     render() {
         return (
             <div className="col-lg-4 col-md-6 mb-4" key={this.props.id}>
-                <a href={"/farmerspage/?" + this.props.userId} >
-                <div className="card h-100">
-                    <a href="#"><img className="card-img-top" src={this.props.img} alt={this.props.item} /></a>
-                    <div className="card-body">
-                    <h4 className="card-title">
-                        {this.props.item}
-                    </h4>
-                    <h5>{this.props.price}</h5>
-                    <p className="card-text">{this.props.description}</p>
-                    </div>
-                    <div className="card-footer">
-                        {this.props.isDashboard ? <div>
-                                                    <div className = "btn btn-primary edit-button w-100" onClick={this.props.modalOpen} data-id={this.props.id}> Edit </div>
-                                                    <div className = "btn delete-button" onClick={this.props.deleteProduct} data-id={this.props.id}><i className="far fa-times-circle"></i></div>
-                                                  </div>
-                                                : (null)}
-                    </div>
-                </div>
-                </a>
+                {this.props.isDashboard ? (<div className="card h-100">
+                                            <img className="card-img-top" src={this.props.img} alt={this.props.item} />
+                                            <div className="card-body">
+                                            <h4 className="card-title">
+                                                {this.props.item}
+                                            </h4>
+                                            <h5>{this.props.price}</h5>
+                                            <p className="card-text">{this.props.description}</p>
+                                            </div>
+                                            <div className="card-footer">
+                                                {this.props.isDashboard ? <div>
+                                                                            <div className = "btn btn-primary edit-button w-100" onClick={this.props.modalOpen} data-id={this.props.id}> Edit </div>
+                                                                            <div className = "btn delete-button" onClick={this.props.deleteProduct} data-id={this.props.id}><i className="far fa-times-circle"></i></div>
+                                                                        </div>
+                                                                        : (null)}
+                                            </div>
+                                        </div>)
+                                        :(<a href={"/farmerspage/?" + this.props.userId}>
+                                            <div className="card h-100">
+                                                <a href="#"><img className="card-img-top" src={this.props.img} alt={this.props.item} /></a>
+                                                <div className="card-body">
+                                                <h4 className="card-title">
+                                                    {this.props.item}
+                                                </h4>
+                                                <h5>{this.props.price}</h5>
+                                                <p className="card-text">{this.props.description}</p>
+                                                </div>
+                                                <div className="card-footer">
+                                                    {this.props.isDashboard ? <div>
+                                                                                <div className = "btn btn-primary edit-button w-100" onClick={this.props.modalOpen} data-id={this.props.id}> Edit </div>
+                                                                                <div className = "btn delete-button" onClick={this.props.deleteProduct} data-id={this.props.id}><i className="far fa-times-circle"></i></div>
+                                                                              </div>
+                                                                            : (null)}
+                                                </div>
+                                            </div>
+                                            </a>)}
+                
             </div>
         )
     }
