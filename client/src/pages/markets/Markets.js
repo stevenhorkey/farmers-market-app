bc17314a91870a84bbd5cdfb699955ad711ba93c
killianmh <killianmh@gmail.com>
2018-07-10 10:07:53 -0700

    fixed market card formatting

--- client/src/pages/markets/Markets.js
+++ client/src/pages/markets/Markets.js
@@ -9,11 +9,25 @@ class Markets extends Component {
 
     state = {
         loading: true,
-        market: []
+        market: [],
+        farmers: []
+    }
+
+    getUrlVars()
+    {
+      var vars = {}, hash;
+      var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1);
+      for(var i = 0; i < hashes.length; i++)
+      {
+          hash = hashes[i].split('=');
+          vars[hash[0]] = hash[1];
+      }
+      return hashes;
     }
 
     componentDidMount() {
         console.log(this.state)
+        let queryNumber = this.getUrlVars();
         axios.get('/api/populateMarketPage/')
             .then((res) => {
                 // console.log(res)
