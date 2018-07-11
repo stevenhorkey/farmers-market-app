835f71fe07f6191af7fe815ac68ccd829ec249b2
stevenhorkey <stevenhorkey@gmail.com>
2018-07-11 14:06:40 -0700

    notification works for farmer requests

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import './Sidebar.css';
 
 class Sidebar extends Component{
 
     render(){
+        console.log(this.props.requests.length)
         return(
             <div className="list-group">
                 {this.props.links.map(link => {
@@ -16,7 +19,21 @@ class Sidebar extends Component{
                         args = []
                     }
                     
-                    return(<a href="/#" className="list-group-item" onClick = {(e) => {parentFunction(e, ...args)}}>{link.name}</a>)
+                    return(
+                        <Fragment>
+                            <a href="/#" className="list-group-item h-100" onClick = {(e) => {parentFunction(e, ...args)}}>
+                                <span className=''>{link.name}</span>
+                                {link.name === 'Manage Vendors' && this.props.requests.length > 0 ? 
+                                        (<span id='sidebar-notification' class="fa-stack fa-1x float-right">
+                                            <i id='notification-circle' class="fa fa-circle fa-stack-1x"></i>
+                                            <strong id='notification-number' class="fa-stack-1x text-white">{this.props.requests.length}</strong>
+                                        </span>)
+                                : (null)
+                                }
+                            </a>
+                            
+                        </Fragment>
+                    )
                     })
                 }
             </div>
