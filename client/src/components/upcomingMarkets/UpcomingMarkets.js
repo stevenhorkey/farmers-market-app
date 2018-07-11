976b877688a3e9f5367083745e22b58bdb2b584a
killianmh <killianmh@gmail.com>
2018-07-10 18:51:35 -0700

    hardcode featured market images

--- client/src/components/upcomingMarkets/UpcomingMarkets.js
+++ client/src/components/upcomingMarkets/UpcomingMarkets.js
@@ -40,26 +40,29 @@ class UpcomingMarkets extends Component {
                     <div className="row">
                         <div className="col-lg-8 col-md-10 mx-auto">
                             <div className="upcoming-markets-heading text-center">
-                                <h1 className='bhs text-white'>Upcoming Markets</h1>
+                                <h1 className='bhs text-white'>Featured Markets</h1>
                                 <span className="subheading"></span>
                             </div>
                             <div>
                                 <MarketCard
-                                    marketName='Sata Rita Market'
-                                    marketLocation='1111 W Rd'
-                                    marketTime='11:00AM'
+                                    marketName='Santa Rita Market'
+                                    marketLocation='1111 W Santa Rita Rd'
+                                    marketTime='Saturdays 11:00AM - 7:00PM'
+                                    marketImage='https://d3u03kk87rjfaq.cloudfront.net/wp-content/uploads/2009/06/03145943/farmers-markets.jpg'
                                     isUpcomingMarkets={true}
                                 />
                                 <MarketCard
                                     marketName='Pueblo Market'
-                                    marketLocation='2222 W Rd'
-                                    marketTime='9:00AM'
+                                    marketLocation='2222 E Casa Rd'
+                                    marketTime='Monday-Friday 11:00AM - 5:00PM'
+                                    marketImage='https://images.unsplash.com/photo-1519397028973-5dbbdaecd2f8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e71af403f1fea800733284e2e0711d4f&auto=format&fit=crop&w=1050&q=80'
                                     isUpcomingMarkets={true}
                                 />
                                 <MarketCard
                                     marketName='Rice Market'
-                                    marketLocation='3333 W Rd'
-                                    marketTime='10:00AM'
+                                    marketLocation='3333 W Campbell Rd'
+                                    marketTime='Thursday, Friday-Sunday 10:00AM - 8:00 PM'
+                                    marketImage='https://images.unsplash.com/photo-1524584830732-b69165ddba9a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bdf112f19b1f9e4c1fe8e24272a85a52&auto=format&fit=crop&w=1919&q=80'
                                     isUpcomingMarkets={true}
                                 />
                             </div>
