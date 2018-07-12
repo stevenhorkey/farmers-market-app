d9a55817bcbe4ac17030ecc92c8fe39fe1ab98d2
stevenhorkey <stevenhorkey@gmail.com>
2018-07-12 16:45:38 -0700

    market adresses and build

--- client/src/components/upcomingMarkets/UpcomingMarkets.js
+++ client/src/components/upcomingMarkets/UpcomingMarkets.js
@@ -49,7 +49,7 @@ class UpcomingMarkets extends Component {
                                 <Slide left>
                                     <MarketCard
                                         marketName='Santa Rita Market'
-                                        marketLocation='1111 W Santa Rita Rd'
+                                        marketLocation='1327 W Santa Rita Rd'
                                         marketTime='Saturdays 11:00AM - 7:00PM'
                                         marketImage='https://d3u03kk87rjfaq.cloudfront.net/wp-content/uploads/2009/06/03145943/farmers-markets.jpg'
                                         isUpcomingMarkets={true}
@@ -58,7 +58,7 @@ class UpcomingMarkets extends Component {
                                 <Slide right>
                                     <MarketCard
                                         marketName='Pueblo Market'
-                                        marketLocation='2222 E Casa Rd'
+                                        marketLocation='785 E Casa Rd'
                                         marketTime='Monday-Friday 11:00AM - 5:00PM'
                                         marketImage='https://images.unsplash.com/photo-1519397028973-5dbbdaecd2f8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e71af403f1fea800733284e2e0711d4f&auto=format&fit=crop&w=1050&q=80'
                                         isUpcomingMarkets={true}
@@ -67,7 +67,7 @@ class UpcomingMarkets extends Component {
                                 <Slide left>
                                     <MarketCard
                                         marketName='Rice Market'
-                                        marketLocation='3333 W Campbell Rd'
+                                        marketLocation='8934 W Campbell Rd'
                                         marketTime='Thursday, Friday-Sunday 10:00AM - 8:00 PM'
                                         marketImage='https://images.unsplash.com/photo-1524584830732-b69165ddba9a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bdf112f19b1f9e4c1fe8e24272a85a52&auto=format&fit=crop&w=1919&q=80'
                                         isUpcomingMarkets={true}
